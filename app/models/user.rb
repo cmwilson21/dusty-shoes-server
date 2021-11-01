2a06446bae4cf31027775a57977e16bc61bce407
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2021-11-01 11:28:33 -0500

    cleaned up code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   has_many :trips
-  # has_many :journals, through: :trips
   validates :email, uniqueness: {case_sensitive: false} 
   validates_presence_of :first_name, :last_name, :password_digest, :email
   has_secure_password
